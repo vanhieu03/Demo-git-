cad435b35895544a5f46844e3657896dec999e3c
vanhieu03 <vanhieuzat2003@gmail.com>
2022-10-06 22:48:37 +0700

    fibonacci

--- code ne/Sieu du an/Sieu du an/Sieu du an.cpp
+++ code ne/Sieu du an/Sieu du an/Sieu du an.cpp
@@ -1,7 +1,21 @@
 #include<iostream>
 using namespace std;
+int fib(int n)
+{
+	if (n == 1 || n == 0)
+		return n;
+	else
+		return fib(n - 1) + fib(n - 2);
+}
 int main()
 {
-	cout << "Hello word" << endl;
+	int n;
+	cout << "Nhap so luong cac so trong chuoi fibonacci: ";
+	cin >> n;
+	cout << n << " so dau trong day fibonacci dau tien : " << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << fib(i) << endl;
+	}
 	return 0;
 }
